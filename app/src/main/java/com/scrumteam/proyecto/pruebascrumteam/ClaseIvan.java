c8658813d32122bcc891a1971a968e8011de3d9a
Ivan <ivan_2490@hotmail.com.ar>
2020-05-21 00:10:55 -0300

    segundo commit ivan

---
+++ app/src/main/java/com/scrumteam/proyecto/pruebascrumteam/ClaseIvan.java
@@ -0,0 +1,12 @@
+package com.scrumteam.proyecto.pruebascrumteam;
+
+public class ClaseIvan {
+
+    public ClaseIvan() {
+    }
+
+    public void metodo1Ivan()
+    {
+        int a = 20;
+    }
+}
